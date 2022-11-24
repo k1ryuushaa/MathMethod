22b13cb9c1931cf9b836f1a3818f2c7a7e6a36a2
k1ryuushaa <111593375+k1ryuushaa@users.noreply.github.com>
2022-11-24 11:53:06 +0500

    Delete stepen.js

--- stepen.js
+++
@@ -1,6 +0,0 @@
-function stepen(){
-    var ch1 =document.getElementById('chislo').value;
-    var ch2 =document.getElementById('step').value;
-    var res = Math.pow(Number(ch1),Number(ch2));
-    document.getElementById('otvet').innerHTML="Ответ: "+String(res);
-};
